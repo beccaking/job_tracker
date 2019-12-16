c2c72ca96d03082ac456151a99e18982028094d5
Becca King <becca.king3232@gmail.com>
2019-12-16 14:34:29 -0500

    build folder added in

---
+++ job_tracker/build/precache-manifest.5106117cef63b4a1b2c7a63225ed9af7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0f050fa316e9e903534c07ed63a41cc7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ff7b253fa18528b9022b",
+    "url": "/static/css/main.1e6dc119.chunk.css"
+  },
+  {
+    "revision": "ce53a590a04978cc576a",
+    "url": "/static/js/2.1d49e325.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.1d49e325.chunk.js.LICENSE"
+  },
+  {
+    "revision": "ff7b253fa18528b9022b",
+    "url": "/static/js/main.e218190e.chunk.js"
+  },
+  {
+    "revision": "8e9ec7c5ce4beb756f31",
+    "url": "/static/js/runtime-main.74685f8f.js"
+  }
+]);
